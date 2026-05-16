4cd77b6ed541fedf221493b4d647df7703d49623
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2026-05-16 09:50:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title("Proyecto final UCG")
 st.sidebar.title("Parámetros")
-st.image("logopython.png")
+st.sidebar.image("logopython.png")
+file= st.file_uploader("Chose")
